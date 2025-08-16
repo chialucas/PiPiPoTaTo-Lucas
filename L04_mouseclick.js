efa641d23a0f96d363090c8336c71c1e05b2f1b0
chialucas <chialucas56@gmail.com>
2025-08-16 07:15:15 +0000

    Aug 16, 2025, 7:15 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -6,24 +6,32 @@ let x1,y1,x2, width,height
 
 
 
-
 function setup(){
     createCanvas(400,400)
     background(200);
 }
-
-function draw(){
-    fill(shapeColor);
-    ellipse(200,200,80,80)
-}
+    
+// function draw(){
+//     fill(shapeColor);
+//     ellipse(200,200,80,80)
+// }
 
 function mousePressed(){
     shapeColor = color(random(255), random(255),random(255))
+    circlesize = 20
 }
 
 function mouseReleased(){
     shapeColor = color(random(255), random(255),random(255))
 }
 
+function mouseDragged(){
+    fill(shapeColor);
+    ellipse(mouseX,mouseY,80,80)
+}
 
+
+function mouseMoved(){
+    shapeColor
+}
 
