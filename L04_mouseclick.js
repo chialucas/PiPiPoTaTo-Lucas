42dbc0a53a2078d67221fd26c0ab22c549e78973
chialucas <chialucas56@gmail.com>
2025-08-16 06:28:50 +0000

    Aug 16, 2025, 6:28 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -11,7 +11,7 @@ function draw(){
 }
 
 function mousePressed(){
-    shapecolour = colour(random(255),random(255))
+    shapecolour = colour(random(255),random(255),random(255))
 }
 
 function mouseReleased(){
