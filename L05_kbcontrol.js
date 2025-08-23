f889667c7bd498182fe8ef2ee02d0b65899622c2
chialucas <chialucas56@gmail.com>
2025-08-23 07:20:52 +0000

    Aug 23, 2025, 7:20 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -32,6 +32,6 @@ function draw() {
     //circle(width/2, height/2, 100);
     for(let i=0; i<5; i++) {
         fill(0, i*50, 0);
-        circle(50 + i*50)
+        circle(50 + i*50, 50 + i*50 ,50)
     }
 }
