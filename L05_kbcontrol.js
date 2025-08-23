0e331743cc999aabce04189a451b8f35ea23b86f
chialucas <chialucas56@gmail.com>
2025-08-23 07:26:14 +0000

    Aug 23, 2025, 7:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,37 +1,39 @@
-// write your codes here
-// let shapecolour = "blue";
-let circlesize = 5;
+// // write your codes here
+// // let shapecolour = "blue";
+// let circlesize = 5;
 
-function setup() {
-    createCanvas(400,400)
-    background(220);
-    noStroke();
-}
-
-function mousePressed() {
-    fill(random(255), random(255), random(255));
-}
-
-function mouseDragged() {
-    circle(mouseX, mouseY, circlesize);
-    ellipse(mouseX,mouseY,circlesize,circlesize);
-}
-
-function draw() {
-}
-
-// function setup(){
-//     createCanvas(750,750)
-//     background(200);
+// function setup() {
+//     createCanvas(400,400)
+//     background(220);
+//     noStroke();
 // }
-// function draw(){
-//     circle()
-//     circle(300,300,300)
+
+// function mousePressed() {
+//     fill(random(255), random(255), random(255));
 // }
+
+// function mouseDragged() {
+//     circle(mouseX, mouseY, circlesize);
+//     ellipse(mouseX,mouseY,circlesize,circlesize);
+// }
+
 // function draw() {
-//     //circle(width/2, height/2, 100);
-//     for(let i=0; i<5; i++) {
-//         fill(0, i*50, 0);
-//         circle(50 + i*50, 50 + i*50 ,50)
-//     }
 // }
+
+// // function setup(){
+// //     createCanvas(750,750)
+// //     background(200);
+// // }
+// // function draw(){
+// //     circle()
+// //     circle(300,300,300)
+// // }
+// // function draw() {
+// //     //circle(width/2, height/2, 100);
+// //     for(let i=0; i<5; i++) {
+// //         fill(0, i*50, 0);
+// //         circle(50 + i*50, 50 + i*50 ,50)
+// //     }
+// // }
+
+
