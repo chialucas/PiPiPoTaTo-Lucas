d618336e8b720964c82432b705a8be6171beda93
chialucas <chialucas56@gmail.com>
2025-08-23 07:07:59 +0000

    Aug 23, 2025, 7:07 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,23 +1,23 @@
-// write your codes here
-// let shapecolour = "blue";
-let circlesize = 5;
+// // write your codes here
+// // let shapecolour = "blue";
+// let circlesize = 5;
 
-function setup() {
-    createCanvas(400,400)
-    background(220);
-    noStroke();
-}
+// function setup() {
+//     createCanvas(400,400)
+//     background(220);
+//     noStroke();
+// }
 
-function mousePressed() {
-    fill(random(255), random(255), random(255));
-}
+// function mousePressed() {
+//     fill(random(255), random(255), random(255));
+// }
 
-function mouseDragged() {
-    circle(mouseX, mouseY, circlesize);
-    ellipse(mouseX,mouseY,circlesize,circlesize);
-}
+// function mouseDragged() {
+//     circle(mouseX, mouseY, circlesize);
+//     ellipse(mouseX,mouseY,circlesize,circlesize);
+// }
 
-function draw() {
-}
+// function draw() {
+// }
 
 
