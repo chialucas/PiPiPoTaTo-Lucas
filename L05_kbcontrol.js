d0b3237f64753bbd5fa24d060486eb6c4db99e74
chialucas <chialucas56@gmail.com>
2025-08-30 07:29:06 +0000

    Aug 30, 2025, 7:29 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,56 +1,59 @@
-// // write your codes here
-// // let shapecolour = "blue";
-// let circlesize = 5;
+// // // write your codes here
+// // // let shapecolour = "blue";
+// // let circlesize = 5;
 
-// function setup() {
-//     createCanvas(400,400)
-//     background(220);
-//     noStroke();
-// }
+// // function setup() {
+// //     createCanvas(400,400)
+// //     background(220);
+// //     noStroke();
+// // }
 
-// function mousePressed() {
-//     fill(random(255), random(255), random(255));
-// }
+// // function mousePressed() {
+// //     fill(random(255), random(255), random(255));
+// // }
 
-// function mouseDragged() {
-//     circle(mouseX, mouseY, circlesize);
-//     ellipse(mouseX,mouseY,circlesize,circlesize);
+// // function mouseDragged() {
+// //     circle(mouseX, mouseY, circlesize);
+// //     ellipse(mouseX,mouseY,circlesize,circlesize);
+// // }
+
+// // function draw() {
+// // }
+
+// // // function setup(){
+// // //     createCanvas(750,750)
+// // //     background(200);
+// // // }
+// // // function draw(){
+// // //     circle()
+// // //     circle(300,300,300)
+// // // }
+// // // function draw() {
+// // //     //circle(width/2, height/2, 100);
+// // //     for(let i=0; i<5; i++) {
+// // //         fill(0, i*50, 0);
+// // //         circle(50 + i*50, 50 + i*50 ,50)
+// // //     }
+// // // }
+
+
+// let rectSize = 50;
+
+// function setup(){
+//     createCanvas(600, 600)
 // }
 
 // function draw() {
+//     background(220)
+//     rect(width/2 - rectSize/2, height/2 - rectSize/2, rectSize, rectSize);
+// }
+// function keyPressed() {
+//     rectSize = 100;
 // }
 
-// // function setup(){
-// //     createCanvas(750,750)
-// //     background(200);
-// // }
-// // function draw(){
-// //     circle()
-// //     circle(300,300,300)
-// // }
-// // function draw() {
-// //     //circle(width/2, height/2, 100);
-// //     for(let i=0; i<5; i++) {
-// //         fill(0, i*50, 0);
-// //         circle(50 + i*50, 50 + i*50 ,50)
-// //     }
-// // }
+// function keyReleased() {
+//     rectSize =50;
+// }
 
 
-let rectSize = 50;
 
-function setup(){
-    createCanvas(600, 600)
-}
-
-function draw() {
-    background(220)
-    rect(width/2 - rectSize/2, height/2 - rectSize/2, rectSize, rectSize);
-}
-function keyPressed() {
-    rectSize = 100;
-}
-
-function keyReleased() {
-    rectSize =50;
-}
