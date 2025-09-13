17a2cb20bfa78d606d516ea2a1361521749b862e
chialucas <chialucas56@gmail.com>
2025-09-13 07:08:28 +0000

    Sep 13, 2025, 7:08 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,14 +1,14 @@
-function setup(){
-    createCanvas(400,400);
-}
+// function setup(){
+//     createCanvas(400,400);
+// }
 
-let x = 200;
-let y = 200;
-function draw(){
-    background(25);
-    square
-    if(KeyIsDown(UP_ARROW))
-        rect(100,100)
+// let x = 200;
+// let y = 200;
+// function draw(){
+//     background(25);
+//     square
+//     if(KeyIsDown(UP_ARROW))
+//         rect(100,100)
 
-        constrain(X,100, 300);
-}
+//         constrain(X,100, 300);
+// }
