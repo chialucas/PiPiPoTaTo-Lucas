aa5da5b072424055984c39781a79d1c5d03236cc
chialucas <chialucas56@gmail.com>
2025-09-20 06:34:56 +0000

    Sep 20, 2025, 6:34 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,35 +1,35 @@
+// // function setup(){
+// //     createCanvas(400,400);
+// // }
+
+// // let x = 200;
+// // let y = 200;
+// // function draw(){
+// //     background(25);
+// //     square
+// //     if(KeyIsDown(UP_ARROW))
+// //         rect(100,100)
+
+// //         constrain(X,100, 300);
+// // }
+
+// let staticImage, soundEffect;
+
+// function preload() {
+//     staticImage = loadImage('assets/pico-a.png');
+//     soundEffect = loadsound('assets/pop.mp3');
+// }
+
 // function setup(){
-//     createCanvas(400,400);
+//     createCanvas(200,200)
+//     background("lightblue");
+//     image(staticImage,0,0,100,100)
 // }
-
-// let x = 200;
-// let y = 200;
-// function draw(){
-//     background(25);
-//     square
-//     if(KeyIsDown(UP_ARROW))
-//         rect(100,100)
-
-//         constrain(X,100, 300);
+// function keyPressed(){
+//     if (keyCode === 32){
+//         soundEffect.play();
+//     }
 // }
-
-let staticImage, soundEffect;
-
-function preload() {
-    staticImage = loadImage('assets/pico-a.png');
-    soundEffect = loadsound('assets/pop.mp3');
-}
-
-function setup(){
-    createCanvas(200,200)
-    background("lightblue");
-    image(staticImage,0,0,100,100)
-}
-function keyPressed(){
-    if (keyCode === 32){
-        soundEffect.play();
-    }
-}
 
 
 
