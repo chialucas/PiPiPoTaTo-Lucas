1357bd3d98e44db266cc46f150dabac76506e247
chialucas <chialucas56@gmail.com>
2025-09-20 06:51:25 +0000

    Sep 20, 2025, 6:51 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -3,5 +3,5 @@
 let staticImage,soundEffect,backgroundSound;
 function preload(){
     staticImage = loadImage('asset/pico-a.png')
-    soundEffect = loadSound('')
+    soundEffect = loadSound('asset/')
 }
