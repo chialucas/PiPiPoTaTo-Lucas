9a7142335fdaa790032e2ede9d606e9d69855dd4
chialucas <chialucas56@gmail.com>
2025-10-04 06:29:54 +0000

    Oct 4, 2025, 6:29 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,69 +1,97 @@
-// // write your codes here
+// // // write your codes here
 
-// let staticImage,soundEffect,backgroundSound;
-// let x = 50;
-// let y = 50;
-// let w = 100;
-// let h = 100;
-// function preload(){
-//     staticImage = loadImage('asset/pico-a.png')
-//     soundEffect = loadSound('asset/pico-a.png')
-//     Image(staticImage,x,y,w,h);
-//     backgroundSound.loop();
-// }
+// // let staticImage,soundEffect,backgroundSound;
+// // let x = 50;
+// // let y = 50;
+// // let w = 100;
+// // let h = 100;
+// // function preload(){
+// //     staticImage = loadImage('asset/pico-a.png')
+// //     soundEffect = loadSound('asset/pico-a.png')
+// //     Image(staticImage,x,y,w,h);
+// //     backgroundSound.loop();
+// // }
 
 
-//     function setup(){
-//     createCanvas(600,600)
-//     backgroundSound('lightblue');
-//     }
+// //     function setup(){
+// //     createCanvas(600,600)
+// //     backgroundSound('lightblue');
+// //     }
     
-//     function draw(){
-//         background('lightblue');
-//         Image(staticImage,x,y,w,h);
-//         if (keyCode === 37){
-//             x-5;
-//         }
-//          if (keyCode === 38){
-//             y-5;
-//         }
-//         if (keyCode === 39){
-//             x+5;
-//         }
-//         if (keyCode === 40){
-//             y+5
-//         }
+// //     function draw(){
+// //         background('lightblue');
+// //         Image(staticImage,x,y,w,h);
+// //         if (keyCode === 37){
+// //             x-5;
+// //         }
+// //          if (keyCode === 38){
+// //             y-5;
+// //         }
+// //         if (keyCode === 39){
+// //             x+5;
+// //         }
+// //         if (keyCode === 40){
+// //             y+5
+// //         }
     
-//     }  
+// //     }  
 
-//     function keyPressed(){
-//         if (keyCode === 32){
-//             // sound
-//             w = 50;
-//         }
-//     }
-//     function keyReleased(){
-//         w = 100;
-//     }
+// //     function keyPressed(){
+// //         if (keyCode === 32){
+// //             // sound
+// //             w = 50;
+// //         }
+// //     }
+// //     function keyReleased(){
+// //         w = 100;
+// //     }
 
 let ballX = 300;
 let ballY = 200;
-let ballSize = 30;
-let ballspeedX = 2;
-let ballspeedY = 2;
+let ballSize = 5;
+let ballspeedX = 20;
+let ballspeedY = 20;
+
+// function setup(){
+//     createCanvas(600,400);
+// }
+
+// function draw(){
+//     background(220);
+//     circle(ballX,ballY,ballSize,ballSize);
+    
+//     ballX += ballspeedX
+//     ballY = ballY + ballspeedY;
+
+//     noStroke();
+//     fill(349)
+//     circle(ballX,ballY,ballSize);
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 function setup(){
     createCanvas(600,400);
+    background(220);
 }
 
+
 function draw(){
-    background(220);
-    circle(ballX,ballY,ballSize,ballSize);
-    
-    ballX += ballspeedX
+    ballX += ballspeedX;
     ballY = ballY + ballspeedY;
 
-    noStroke();
-    fill(349)
-    circle(ballX,ballY,ballSize);
+
+    if(ballX - ballSize/2 <=0 || ballX + ballSize/2 >= width){
+        ballspeedX = ballspeedX * -1;
+        fill(random(255),random(255),random(255))
+    }
+
+    if(ballY -ballSize/2 <=0 || ballY + ballSize/2 >= height){
+        ballspeedY *= -1; 
+        fill(random(255),random(255),random(255)); 
+    }
+
+noStroke();
+//  fill(349);
+circle(ballX,ballY,ballSize);
 }
--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,1 +1,19 @@
-// write your codes here
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     textSize(100);
+//     Text("hello I am lucas and i am 13.5690783 years in chinese years.")
+// }
+
+// let favouriteFoots = ["","", "","", ""];
+
+// for (let i = 0, u < favouriteFoots.length; i++) {
+//     console.log(favouriteFoots[i]);
+// }
+
+
+
+
+
+
+
