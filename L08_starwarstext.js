d6c0f9c94a0531202ecc661abcf0d05d81c61d5b
chialucas <chialucas56@gmail.com>
2025-10-04 06:39:56 +0000

    Oct 4, 2025, 6:39 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,33 +1,39 @@
+// // function setup(){
+// //     createCanvas(400,400);
+// //     background(220);
+// //     textSize(100);
+// //     Text("hello I am lucas and i am 13.5690783 years in chinese years.")
+// // }
+
+// // let favouriteFoots = ["","", "","", ""];
+
+// // for (let i = 0, u < favouriteFoots.length; i++) {
+// //     console.log(favouriteFoots[i]);
+// // }
+
+
+
+
+
+
+
+
+
+// let favouriteFoots = ["waterbotel","bugger","suesheesh","Toecoes","poostasasa"]
+// let yPos = 200;
 // function setup(){
 //     createCanvas(400,400);
 //     background(220);
-//     textSize(100);
-//     Text("hello I am lucas and i am 13.5690783 years in chinese years.")
+//     textSize(34);
+//     textAlign(RIGHT, CENTER);
+// }
+// function draw(){
+//     background(220);
+//     //fill(0);
+//     for(let i = 0, i<favouriteFoots.length; i++){
+//         text((i +1)) + "." + favouriteFoots[1],300,(80+i*30) + yPos);
+//     }
 // }
 
-// let favouriteFoots = ["","", "","", ""];
-
-// for (let i = 0, u < favouriteFoots.length; i++) {
-//     console.log(favouriteFoots[i]);
-// }
 
 
-
-
-
-
-
-
-
-let favouriteFoots = ["waterbotel","bugger","suesheesh","Toecoes","poostasasa"]
-let yPos = 200;
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    textSize(34);
-    textAlign(RIGHT, CENTER);
-}
-function draw(){
-    background(220);
-    //fill(0);
-}
