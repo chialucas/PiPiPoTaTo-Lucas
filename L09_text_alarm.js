057ca7ab3b7ea36f6a48a1d5d267e26b623f0efe
chialucas <chialucas56@gmail.com>
2025-10-11 07:00:09 +0000

    Oct 11, 2025, 7:00 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,51 +1,76 @@
-// // let bounce = "bouncing!";
-// // let bouncex = 200;
+// // // let bounce = "bouncing!";
+// // // let bouncex = 200;
+
+// // // function setup(){
+// // //     createCanvas(600,600)
+// // //     bouncex = width;
+// // // }
+
 
 // // function setup(){
-// //     createCanvas(600,600)
-// //     bouncex = width;
+// //     createCanvas(600,400)
+// //     textAlign(CENTER,CENTER)
+// // }
+// // function draw(){
+// //     background(400,600)
+// //     let h = hour();
+// //     let m = minute();
+// //     let s = second();
+
+
+// //     let StringTime = nf(h,2) + ":" + nf(m,2) + ":" + nf(s,2)
+// //     fill(0);
+// //     textSize(46);
+// //     text(StringTime, width/2 , height/2);
 // // }
 
 
+
+
+// setInterval()
+
+// let interval()
+
 // function setup(){
-//     createCanvas(600,400)
-//     textAlign(CENTER,CENTER)
+//     createCanvas(600,400);
+//     textAlign(CENTER,CENTER);
 // }
+
 // function draw(){
-//     background(400,600)
+//     background(600,400);
+
 //     let h = hour();
 //     let m = minute();
 //     let s = second();
 
+//     let stringTime = (h, 2) + ":" + Nf(m,2) + ":" + nf(s,2);
+//     fill(0)
+//     textSize(32);
 
-//     let StringTime = nf(h,2) + ":" + nf(m,2) + ":" + nf(s,2)
-//     fill(0);
-//     textSize(46);
-//     text(StringTime, width/2 , height/2);
+//     text
+
+//     text("click me to start the timer", width/2, height - 50);
+
+//     textSize(100);
+//     text(countdown, width /2, height/2 + 20);
+// }
+
+// function mouseDraw(){
+//     interval = setinterval(updateCountdown, 1000);
+// }
+// function updateCountdown(){
+//     if(countdown > 0){
+//         countdown-=1;
+//     }else{
+//         clearinterval(interval)
+//     }
 // }
 
 
 
 
-setInterval()
-
-let intervalid
-
-function setup(){
-    createCanvas(600,400)
-    textAlign(CENTER,CENTER)
-}
+//     let h = hour()
+//     let m = minute()
+//     let s = second()
 
 
-function setTimer(){
-    let timer = 10
-
-}
-
-function draw(){
-    background(600,400)
-
-    let h = hour()
-    let m = minute()
-    let s = second()
-}
