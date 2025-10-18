f2812efceccea9be39020375741cf73951a66147
chialucas <chialucas56@gmail.com>
2025-10-18 07:18:39 +0000

    Oct 18, 2025, 7:18 AM

--- L10_name.js
+++ L10_name.js
@@ -3,41 +3,47 @@
 
 
 
-let countdown = 5;
-let timerStarted = false;
+// let countdown = 5;
+// let timerStarted = false;
+// let bgcolor = 220;
 
+// function setup(){
+//     createCanvas(600,400)
+//     background(67,67)
+//     textAlign(CENTER,CENTER)
+// }
 
-function setup(){
-    createCanvas(600,400)
-    background(67,67)
-    textAlign(CENTER,CENTER)
-}
-
-    interval = setInterval(updateCountdown, 1000)
-
-
-
-function draw(){
-    background(bscolor)
-    text(countdown, width/2)
-}
+//     interval = setInterval(updateCountdown, 1000)
 
 
 
-let interval;
-let bgcolor;
-
-
-function updateCountdown(){
-    if(!timerStarted){
-        userStartAudio();
-        timerStarted = true;
-        interval = setInterval(updateCountdown, 1000);
-
-    }
-}
+// function draw(){
+//     background(bscolor)
+//     text(countdown, width/2, height/2+20)
+//     if(!timerStarted){
+//         userStartAudio();
+//         timerStarted = true;
+//         interval = setInterval(updateCountdown, 1000);
+// }
 
 
 
-// six seven 6 7 6 7 mango mango MUSTARDDDD
-//           🫴🫴
+// let interval;
+// let bgcolor;
+
+
+// function updateCountdown(){
+//     if(countdown>0 { 
+//         Countdown-=1;
+//         bgcolor = color(random(255),random(255),random(255));
+//         console.log(countdown);
+//     }
+// )
+
+//     }
+// }
+
+
+
+// // six seven 6 7 6 7 mango mango MUSTARDDDD
+// //           🫴🫴
