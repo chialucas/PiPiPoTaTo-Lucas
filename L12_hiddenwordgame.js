2af10397876f28391be5d62b8dd93260db812a22
chialucas <chialucas56@gmail.com>
2025-11-08 07:30:49 +0000

    Nov 8, 2025, 7:30 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -15,7 +15,7 @@ function setup(){
     inputField.position(55,20);
 
     displayText = randomword[0].toUpperCase() + " " + ""
-    text("HINT:")
+    text("HINT: " + displayText)
 
     submitButton = createButton("show");
     submitButton.size(150,30);
