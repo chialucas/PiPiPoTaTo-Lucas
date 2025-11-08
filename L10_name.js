5a4b665f9b4c4f94968fe768c152a1724cb0c0f0
chialucas <chialucas56@gmail.com>
2025-11-08 06:47:44 +0000

    Nov 8, 2025, 6:47 AM

--- L10_name.js
+++ L10_name.js
@@ -3,56 +3,113 @@
 
 
 
-// let countdown = 5;
-// let timerStarted = false;
-// let bgcolor = 220;
+// // let countdown = 5;
+// // let timerStarted = false;
+// // let bgcolor = 220;
+
+// // function setup(){
+// //     createCanvas(600,400)
+// //     background(67,67)
+// //     textAlign(CENTER,CENTER)
+// // }
+
+// //     interval = setInterval(updateCountdown, 1000)
+
+
+
+// // function draw(){
+// //     background(bscolor)
+// //     text(countdown, width/2, height/2+20)
+// //     if(!timerStarted){
+// //         userStartAudio();
+// //         timerStarted = true;
+// //         interval = setInterval(updateCountdown, 1000);
+// // }
+
+
+
+// // let interval;
+// // let bgcolor;
+
+
+// // function updateCountdown(){
+// //     if(countdown>0 { 
+// //         Countdown-=1;
+// //         bgcolor = color(random(255),random(255),random(255));
+// //         console.log(countdown);
+// //     }
+// // )
+
+// //     }
+// // }
+
+// let inputText;
+// let userText = "Text here"
 
 // function setup(){
 //     createCanvas(600,400)
-//     background(67,67)
-//     textAlign(CENTER,CENTER)
-// }
 
-//     interval = setInterval(updateCountdown, 1000)
-
-
-
-// function draw(){
-//     background(bscolor)
-//     text(countdown, width/2, height/2+20)
-//     if(!timerStarted){
-//         userStartAudio();
-//         timerStarted = true;
-//         interval = setInterval(updateCountdown, 1000);
+//     inputText = createInput();
+//     inputText.position(200, height -80);
+//     inputText.
 // }
 
 
 
-// let interval;
-// let bgcolor;
 
-
-// function updateCountdown(){
-//     if(countdown>0 { 
-//         Countdown-=1;
-//         bgcolor = color(random(255),random(255),random(255));
-//         console.log(countdown);
-//     }
-// )
-
-//     }
-// }
 
 let inputText;
-let userText = "Text here"
+let userText;
+let colorPicker;
+
+
 
 function setup(){
-    createCanvas(600,400)
+    createCanvas(600,400);
+    inputText
 
-    inputText = createInput();
-    inputText.position(200, height -80);
-    inputText.
+
+    colorPicker = createColorPicker("hsla(233, 100%, 50%, 1.00)");
+    colorPicker.position(200, 32)
 }
 
-// // six seven 6 7 6 7 mango mango MUSTARDDDD
-// //           🫴🫴
+function draw() {
+    background(colorPicker.value());
+
+    fill(255)
+    Rect(50,100,500,160,10);
+    fill(0)
+    textSize(16);
+    textAlign(LEFT);
+    text(userText,width/2,180)
+    text(ageValue,width/2,220)
+
+    text("Pick a big back grounders color", 20, 50);
+    text("Name:", 100,height - 100);
+    text("Age:", 100, height - 80);
+
+
+    textSize(16);
+    textAlign(LEFT);
+    fill(0);
+
+
+}
+
+function updateText () {
+    userText = this.value()
+}
+
+function updateAge () {
+    userText
+}
+
+
+
+
+
+
+
+
+// // // six seven 6 7 6 7 mango mango MUSTARDDDD
+// // //          🫴🫴
--- L11_madlib.js
+++ L11_madlib.js
@@ -1,1 +1,78 @@
-// write your codes here
+// const { version } = require("react");
+
+let NounField;
+let VerbField;
+let AdjField;
+let AdverbField;
+let placeField;
+let submitButton;
+let outputArea;
+let storyText = "";
+let storyTemplates;
+
+function setup(){
+    createCanvas(600,400)
+    background(220);
+
+    
+
+    NounField = createInput("Noun ");
+    NounField.position(55, 20);
+
+    VerbField = createInput("Verb ");
+    VerbField.position(55,60);
+
+    AdjField = createInput("Adjective ");
+    AdjField.position(55,100);
+
+    AdverbField = createInput("Adverb ");
+    AdverbField.position(55,140);
+
+    placeField = createInput("place ")
+    placeField.position(55, 180);
+
+    submitButton = createCanvas("show");
+    submitButton.position(55,220);
+    submitButton.mousePressed(displayInput);
+
+ }
+
+function displayInput(){
+    console.log(NounField.value());
+    console.log(NounField.value());
+    console.log(NounField.value());
+    console.log(NounField.value());
+    console.log(NounField.value());
+
+    // background(220)
+    // const inputValue = inputField.value()
+    // textSize(64);
+    // textAlign(CENTER, CENTER);
+    // text(inputValue, 200, 100);
+
+}
+
+
+
+
+let storyText = "";
+let storyTemplates;
+
+function setup(){
+    storyTemplates = [
+        "the {adj} {noun} decided to {verb} {adv} at the {place}.",
+        "One day, a {adj} {noun} wanted to {verb} {adv} in {place.",
+        "Did you hear about the {adj} {noun} that tried to {verb} {adv} near {place}"
+
+    ];
+
+let template = random(storyTemplates);
+
+
+storyTexts = template.replace("{noun}","dog")
+                     .replace("{verb}","jump")
+                     .replace("{adj}","happy")
+                     .replace("{adv}","happily")
+
+console.log(storyTexts)
+}
